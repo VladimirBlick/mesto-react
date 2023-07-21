3b2df4d350f88bf82cfb88f5b651f5500c13994a
Vladimir BLick <blick3@yandex.ru>
2023-07-21 16:23:17 +0500

    pr10/ready8

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,9 @@
-export default function Card({card, onCardClick}) {
+export default function Card({ card, onCardClick }) {
     return (
         <div className="element">
             <button type="button" className="element_delete" />
-            <img src={card.link} alt = {`Изображение ${card.name}`} 
-            className="element__image" onClick={()=> onCardClick({link:card.link, name:card.name})}/>
+            <img src={card.link} alt={`Изображение ${card.name}`}
+                className="element__image" onClick={() => onCardClick({ link: card.link, name: card.name })} />
             <div className="element__city">
                 <h2 className="element__city-name"> {card.name}</h2>
                 <div className="element__city-like-container">
